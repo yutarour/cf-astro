9dcb8f6d7545efaf329b0ae1487161672f679f8e
Yutaro U. <yutaro_u@outlook.com>
2025-04-30 20:39:30 -0700

    Update astrowind

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,5 @@
-type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
-
-declare namespace App {
-  interface Locals extends Runtime {}
-}
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+/// <reference types="vite/client" />
+/// <reference types="../vendor/integration/types.d.ts" />
